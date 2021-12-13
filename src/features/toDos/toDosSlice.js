4e933e06c65c85d96ff2fea6e815465578e9903d
Laura Ferrando Ferrero <laura@workpath.com>
2021-12-13 23:20:45 +0100

    refactor: remove redux logic from slice

--- src/features/toDos/toDosSlice.js
+++ src/features/toDos/toDosSlice.js
@@ -1,43 +1,4 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-
 import { fetchToDos } from './toDosAPI';
-
-const initialState = {
-  items: [],
-  status: 'idle',
-};
-
-export const getTodos = createAsyncThunk('toDos/getToDos', async () => {
-  const { data } = await fetchToDos();
-  return data;
-});
-
-export const toDosSlice = createSlice({
-  name: 'toDos',
-  initialState,
-  reducers: {
-    addToDo(state, action) {
-      state.items.push(action.payload);
-    },
-    toggleToDo(state, action) {
-      const toDoId = action.payload;
-      const modifiedToDo = state.items.find((toDo) => toDo.id === toDoId);
-      modifiedToDo.isCompleted = !modifiedToDo.isCompleted;
-    },
-  },
-  extraReducers(builder) {
-    builder
-      .addCase(getTodos.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(getTodos.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.items = state.items.concat(action.payload);
-      });
-  },
-});
-
-export const { addToDo, toggleToDo } = toDosSlice.actions;
 
 export const createToDosSlice = (set) => ({
   toDos: [],
